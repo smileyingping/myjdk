9529d5b221ccd07c85cf6356f2dd9e9c5f3b13fd
nieyingping <smileyingping@163.com>
2017-11-10 09:33:53 +0800

    初始化项目

---
+++ src/java/util/MyArraylist.java
@@ -0,0 +1,163 @@
+package java.util;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+public class MyArraylist<T> implements Iterable<T> {
+
+	private final static int INIT_COUNT = 10;
+
+	private int size = 0;
+
+	private transient int modcount;
+
+	Object[] arrays;
+
+	public MyArraylist() {
+		this(INIT_COUNT);
+	}
+
+	public MyArraylist(int count) {
+		arrays = new Object[count];
+	}
+
+	public void add(Object obj, int index) {
+		checkIndexRangeForAdd(index);
+		checkIndex(size + 1);
+		System.arraycopy(arrays, index, arrays, index + 1, size - index);
+		arrays[index] = obj;
+		size++;
+	}
+
+	public boolean add(Object obj) {
+		checkIndex(size + 1);
+		modcount++;//加1
+		arrays[size++] = obj;
+		return true;
+	}
+
+	public void checkIndex(int index) {
+		if (index > arrays.length) {
+			modcount++;//加1
+			int oldLength = size;
+			int newLength = oldLength + (index >> 1);
+			arrays = Arrays.copyOf(arrays, newLength);
+		}
+	}
+
+	public T get(int index) {
+		checkIndexRangeForAdd(index);
+		return (T) arrays[index];
+	}
+
+	public T remove(int index) {
+		checkIndexRange(index);
+		modcount++;//加1
+
+		@SuppressWarnings("unchecked")
+		T t = (T) arrays[index];
+		int moveLength = size - index - 1;
+		if (moveLength > 0) {
+			System.arraycopy(arrays, index + 1, arrays, index, moveLength);
+		}
+		arrays[--size] = null;
+		return t;
+	}
+
+	public T set(Object obj, int index) {
+		checkIndexRange(index);
+		modcount++;//加1
+		T t = (T) arrays[index];
+		arrays[index] = obj;
+		return t;
+	}
+
+	public void checkIndexRange(int index) {
+		if (index > size) {
+			throw new IndexOutOfBoundsException("数组越界");
+		}
+	}
+
+	public void checkIndexRangeForAdd(int index) {
+		if (index > size || index < 0) {
+			throw new IndexOutOfBoundsException("数组越界");
+		}
+	}
+
+	public static void main(String[] args) {
+		MyArraylist<Integer> lists = new MyArraylist<Integer>();
+		for (int i = 0; i < 15; i++) {
+			lists.add(i);
+			System.out.println(lists);
+		}
+
+		// int i=0;
+		// for (Integer integer : lists){
+		// System.out.println(integer);
+		// if(i == 1){
+		// lists.remove(i);
+		// }
+		// i++;
+		// }
+
+		lists.add(-1, 9);
+		System.out.println(lists);
+
+		lists.set(0, 9);
+		System.out.println(lists);
+
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public String toString() {
+		Iterator<T> it = iterator();
+		if (!it.hasNext())
+			return "[]";
+
+		StringBuilder sb = new StringBuilder();
+		sb.append('[');
+		for (;;) {
+			T e = it.next();
+			sb.append(e == this ? "(this Collection)" : e);
+			if (!it.hasNext())
+				return sb.append(']').toString();
+			sb.append(',').append(' ');
+		}
+	}
+
+	public Iterator<T> iterator() {
+		return new MyArrayListIterator();
+	}
+
+	private class MyArrayListIterator implements Iterator<T> {
+
+		private int count;
+
+		private int exceptCount = modcount;
+
+		public boolean hasNext() {
+			return count != size;
+		}
+
+		public T next() {
+			checkModcount();
+			return (T) arrays[count++];
+		}
+
+		public void checkModcount() {
+			if (exceptCount != modcount) {
+				throw new ConcurrentModificationException();
+			}
+		}
+
+	}
+
+}
---
+++ src/java/util/MyHashMap.java
@@ -0,0 +1,207 @@
+package java.util;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class MyHashMap<K, V> {
+
+	transient Node<K, V>[] table;
+	
+	private static int initCount = 16;//初始化长度
+	
+	private static float loadFator = 0.75f;//加载因子
+	
+	private int size;
+	
+	private static int threshold;//扩容临界值
+	
+	
+	static final int MAXIMUM_CAPACITY = 1 << 30;
+	
+	public MyHashMap(){
+		this(initCount, loadFator);
+	}
+	
+	public MyHashMap(int initCount){
+		this(initCount, loadFator);
+	}
+	
+	public MyHashMap(int initCount, float loadFator){
+		if(initCount < 0){
+			throw new IllegalArgumentException("初始化长度不合法 : " + initCount);
+		}
+		
+		if(initCount > MAXIMUM_CAPACITY){
+			initCount = MAXIMUM_CAPACITY;
+		}
+		
+		if(loadFator <0 || Float.isNaN(loadFator)){
+			throw new IllegalArgumentException("加载因子不合法 : " + loadFator);
+		}
+		
+		this.loadFator = loadFator;
+		this.threshold = (int) (initCount * loadFator);
+	}
+	
+	public Object put(K key, V value){
+		if(table == null || table.length == 0){
+			//此时真正创建数组
+			table = new Node[initCount];
+		}
+		int hash = hash(key);
+		int len = table.length;
+		int index = (len-1) & hash;
+		Node<K,V> p;
+		if((p = table[index]) == null){
+			table[index] = new Node<K, V>(hash, key, value, null);
+		}else{//哈希碰撞
+			MyHashMap<K, V>.Node<K, V> e = p;
+			Node<K,V> temp = p;
+			
+			do{
+				if(e.hash == hash && ((e.key == key) || (key != null && key.equals(e.key)))){
+					V oldValue = p.value;
+					//用新值覆盖旧值
+					p.value = value;
+					return oldValue;
+				}
+				temp = e;
+			}while((e = temp.next) != null);
+			//将元素放在链表最前面
+			table[index] = new Node<K, V>(hash, key, value, p);
+		}
+		
+		if(++size > threshold){//扩容
+			resize();
+		}
+		
+		return null;
+	}
+	
+	public V get(K k){
+		int hash = hash(k);
+		int len = table.length;
+		int index = (len-1) & hash;
+		Node<K,V> node = table[index];//找到链表
+		if (node == null) {
+			return null;
+		}
+		Node<K,V> p;
+		if ((p = node.next) == null) {//如果链表只有一个元素，直接返回
+			return node.value;
+		} else {//如果有多个元素，循环比较key
+			p = node;
+			do {
+				if (p.key == k || p.key.equals(k)) {
+					return p.value;
+				}
+				node = p;
+			} while ((p = node.next) != null);
+		}
+		return null;
+	}
+	
+	 
+	public void remove(K k){
+		int hash = hash(k);
+		int len = table.length;
+		int index = (len-1) & hash;
+		
+		Node<K,V> node = table[index];
+		Node<K,V> prev = node;
+		while (node != null) {
+			if(node.hash == hash && (node.key == k || node.key.equals(k))){
+				if(node == prev){//被删除元素在链表第一位
+					table[index] = node.next;
+				}else{
+					prev.next = node.next;//node 为当前元素  prev为前一个元素  将前一个元素的指针next指向下一个元素
+				}
+				size--;
+			}
+			prev = node;
+			node = node.next;
+		};	
+		
+	}
+	
+	public void resize(){
+		int len = size << 1;
+		threshold = (int)(len * loadFator);
+		size = 0;
+		Node<K,V>[] newTable = new Node[len];
+		Node<K,V>[] tempTable = table;
+		table = newTable;
+		
+		int tempSize = tempTable.length;
+		for(int i=0; i<tempSize; i++){
+			Node<K,V> node = tempTable[i];
+			while(node != null){
+				put(node.key, node.value);
+				node = node.next;
+			}
+		}
+	}
+	
+	static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+	
+
+	@SuppressWarnings("hiding")
+	class Node<K, V> implements Map.Entry<K, V> {
+		final int hash;
+
+		final K key;
+		V value;
+
+		Node<K, V> next;
+
+		Node(int hash, K key, V value, Node<K, V> next) {
+			this.hash = hash;
+			this.key = key;
+			this.value = value;
+			this.next = next;
+		}
+
+		@Override
+		public K getKey() {
+			return key;
+		}
+
+		@Override
+		public V getValue() {
+			return value;
+		}
+
+		@Override
+		public V setValue(V value) {
+			V tempValue = value;
+			this.value = value;
+			return tempValue;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + key + " : " + value + "]" + " next " + next;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(key) ^ Objects.hashCode(value);
+		}
+
+		public final boolean equals(Object o) {
+			if (o == this)
+				return true;
+			if (o instanceof Map.Entry) {
+				Map.Entry<?, ?> e = (Map.Entry<?, ?>) o;
+				if (Objects.equals(key, e.getKey()) && Objects.equals(value, e.getValue()))
+					return true;
+			}
+			return false;
+		}
+
+	}
+
+}
